61c8479cabe6fa515f07c05005ac1338084bd2ba
Ferstuque <75496408+Ferstuque@users.noreply.github.com>
2024-08-11 16:35:58 +0000

    commit

--- app.py
+++ app.py
@@ -9,6 +9,7 @@ import plotly.express as px
 
 @st.cache_resource
 def carrega_modelo():
+    #https://drive.google.com/file/d/14USuEnscVJ-10SdOkGNuNndzaCv4KclZ/view?usp=drive_link
     url = 'https://drive.google.com/uc?id=14USuEnscVJ-10SdOkGNuNndzaCv4KclZ'
     gdown.download(url,'modelo_quantizado16bits.tflite')
     interpreter = tf.lite.Interpreter(model_path='modelo_quantizado16bits.tflite')
@@ -17,42 +18,43 @@ def carrega_modelo():
     return interpreter
 
 def carrega_imagem():
-    uploaded_file = st.file_uploader('Arraste e solte uma imagem aqui ou clique para selecionar uma', type=['png', 'jpg', 'jpeg'])
-
+    # Cria um file uploader que permite o usuário carregar imagens
+    uploaded_file = st.file_uploader("Arraste e solte uma imagem aqui ou clique para selecionar uma", type=['png', 'jpg', 'jpeg'])
     if uploaded_file is not None:
+        # Para ler a imagem como um objeto PIL Image
         image_data = uploaded_file.read()
         image = Image.open(io.BytesIO(image_data))
 
+        # Mostrar a imagem carregada
         st.image(image)
-        st.success('Imagem foi carregada com sucesso')
+        st.success("Imagem carregada com sucesso!")
 
+        #Pré-processamento da imagem
         image = np.array(image, dtype=np.float32)
-        image = image / 255.0
+        image = image / 255.0  # Normalização para o intervalo [0, 1]
         image = np.expand_dims(image, axis=0)
 
         return image
     
 def previsao(interpreter,image):
+    # Obtém detalhes dos tensores de entrada e saída
     input_details = interpreter.get_input_details()
     output_details = interpreter.get_output_details()
 
-    interpreter.set_tensor(input_details[0]['index'],image)
+    # Define o tensor de entrada para o modelo
+    interpreter.set_tensor(input_details[0]['index'], image)
 
+    # Executa a inferência
     interpreter.invoke()
 
+    # Obtém a saída do modelo
     output_data = interpreter.get_tensor(output_details[0]['index'])
-    classes = ['BlackMeasles', 'HealthyGrapes', 'LeafBlight', 'BlackRot']
-
+    classes = ['BlackMeasles', 'BlackRot', 'HealthyGrapes', 'LeafBlight']
     df = pd.DataFrame()
     df['classes'] = classes
-    df['probabilidades (%)'] = 100 * output_data[0]
-
-    fig = px.bar(df,
-                 y='classes',
-                 x='probabilidades (%)',
-                 orientation='h',
-                 text='probabilidades (%)',
-                 title='Probabilidade de Classes de Doenças em Uvas')
+    df['probabilidades (%)'] = 100*output_data[0]
+    fig = px.bar(df, y='classes', x='probabilidades (%)', orientation='h', text='probabilidades (%)',
+             title='Probabilidade de Classes de Doenças em Uvas')
     st.plotly_chart(fig)
 
 
@@ -61,17 +63,19 @@ def main():
         page_title="Classifica Folhas de Videira",
         page_icon="🍇",
     )
-
+    
     st.write("# Classifica Folhas de Videira! 🍇")
+    
 
-    #Carrega modelo
-    interpreter=carrega_modelo()
-    #Carrega imagem
-    image=carrega_imagem()
-    #Classifica
+    interpreter = carrega_modelo()
+
+    image = carrega_imagem()
+
     if image is not None:
 
         previsao(interpreter,image)
+    
+
 
 if __name__ == "__main__":
     main()
